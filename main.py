37911c6942769a4d269f2ccafe5f0c004aca957a
Alexander <Sanya19976@gmail.com>
2022-04-21 22:02:14 +0300

    Add tramsmit logs to the server

--- main.py
+++ main.py
@@ -3,25 +3,27 @@ from page import Page
 
 client = Client()
 page = Page()
-while True:
-    folder_from = input("Sync folder from:")
-    folder_to = input('Sync folder to:')
-    print('''
+
+folder_from = input("Sync folder from:")
+folder_to = input('Sync folder to:')
+print('''
 Sync from server: 0
 Sync to server : 1
     ''')
-    if folder_to == "":
-        folder_to =='/media/pi/D/share/BACKUPS'
-    ans = input()
-    if int(ans) == 0:
-        print('Sync...')
-        page.copy_from_server(client, folder_from, folder_to)
-    elif int(ans) == 1:
-        print('Sync...')
-        page.copy_to_server(client, folder_from, folder_to)
-    else:
-        print('Close...')
-        break
+if folder_to == "":
+    folder_to =='/media/pi/D/share/BACKUPS'
+ans = input()
+if int(ans) == 0:
+    print('Sync...')
+    page.copy_from_server(client, client, folder_to)
+    page.send_log_files(client)
+elif int(ans) == 1:
+    print('Sync...')
+    page.copy_to_server(client, folder_from, folder_to)
+    page.send_log_files(client)
+else:
+    print('Close...')
+
 
 #TODO Сделать хранение логов на сервере (Расберри). необходимо создать новые функции которые передадут лог в папку
 #TODO Если удаленно не удается сохранить лог, необходимо сохранить его локально
--- page.py
+++ page.py
@@ -6,6 +6,8 @@ class Page:
         self.files_url_list = list()
         self.dir_list = list()
         self.check = 0
+        self.current_time = 0
+        self.log_file_link = None
 
 #Проверка на существтвание локадьной папки
     def check_local_page(self, page):
@@ -48,35 +50,48 @@ class Page:
     def get_local_page(self, page):
         os.chdir(page)
 
-#Запись даты в лог файла
-    def log_date(self):
-        now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
-        file = open('logs/log.txt','a+')
-        file.write("Current Time = " + current_time+'\n')
-        file.close()
 
 # Создание лога и записи в него переданных файлов
-    def log_files(self,files):
-        file = open('logs/log.txt','a+')
+    def log_files(self, files):
+        now = datetime.now()
+        self.current_time = now.strftime("%H:%M:%S")
+        file = open(f'logs/log_{self.current_time}.txt', 'w')
+        file.write("Current Time = " + self.current_time + '\n')
         file.write(files+'\n')
+        self.log_file_link = os.getcwd() + f"/logs"
         file.close()
 
+    def delete_log_file(self):
+        file = self.walk_local_page(os.getcwd() + "/logs")
+        for i in self.files_url_list:
+            os.remove(i)
+
+    def send_log_files(self,transfer):
+        try:
+            files_list = self.walk_local_page(self.log_file_link)
+            for i in files_list:
+                transfer.put_files(self.log_file_link+"/" +i,
+                                   f'/media/pi/D/share/BACKUPS/logs/log_{self.current_time}.txt')
+        except:
+            print('Error send log file')
+        self.delete_log_file()
 # Обертка копирования информации с локальной папки на удаленное устройство
     def copy_to_server(self, transfer, page_from, page_to):
         files_list = self.walk_local_page(page_from)
         x = 0
-        self.log_date()
         for i in self.files_url_list:
             transfer.put_files(i, page_to + '/' + files_list[x])
             log = i + " - "+ page_to + '/' + files_list[x]
             self.log_files(log)
             x = x + 1
 
-# Обертка копирования информации с уудаленногг устройства в локальную папку
+# Обертка копирования информации с удаленного устройства в локальную папку
     def copy_from_server(self, transfer, page_from, page_to):
         answer = transfer.list(page_from)
-        self.log_date()
+        x=0
         for i in answer:
             transfer.get_files(page_from + '/' + i, page_to + '/' + i)
-            self.log_files(page_from + '/' + i+ " - " + page_to + '/' + i)
+            log = page_from + '/' + i+ " - " + page_to + '/' + i
+            self.log_files(log)
+            x+=1
+
